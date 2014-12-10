6e0479931e2939c682fdc5c1d122ceeb7e5eb77a
ersaxenas <er.saxenas@gmail.com>
2014-12-10 17:04:48 -0500

    Restful functions added

--- dao/zipcodedao.js
+++ dao/zipcodedao.js
@@ -5,7 +5,9 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/gda');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-
+/**
+ * Schema for zipcodes model
+ */
 var zipcodemodel = mongoose.Schema({
     zip: String,
     city: String,
@@ -17,22 +19,46 @@ var zipcodemodel = mongoose.Schema({
 });
 
 var zipcode = mongoose.model('zipcode', zipcodemodel);
-
+/**
+ * Function saves zipcode to database.
+ */
 var savezipcode = function(zipcodes) {
     var mzips = new zipcode(zipcodes);
     mzips.save(function(err){
         if(err) {console.log(err);}
     });
 }
-
+/**
+ * Function pull different states from the database.
+ */
 var getAllStates = function(fn){
     var collection = db.collection('zipcodes');
     zipcode.find().distinct('state',function(err, states){
        fn(err,{states:states});
     });
-
-
 }
 
+/**
+ * Function pulls all the listed cities for a states from the database.
+ */
+var getCityForStates = function(state,fn){
+    var collection = db.collection('zipcodes');
+    zipcode.find().distinct({state:state},'city',function(err, city){
+       fn(err,{city:city});
+    });
+}
+/**
+ * Function pulls all the listed zipcodes for city from the database.
+ */
+var getZipCodesForCity = function(city,fn){
+    var collection = db.collection('zipcodes');
+    zipcode.find({city:city},{zip:1},function(err, city){
+       fn(err,{city:city});
+    });
+}
+
+/*Exposing module functions*/
 module.exports.save = savezipcode;
 module.exports.getAllStates = getAllStates;
+module.exports.getCityForStates = getCityForStates;
+module.exports.getZipCodesForCity = getZipCodesForCity;
--- routes/csvupload.js
+++ routes/csvupload.js
@@ -4,7 +4,9 @@
 var path = require('path');
 var xlsx = require('xlsx');
 var zipcodedao = require('../dao/zipcodedao');
-
+/**
+ *Function uploads the xlsx file to database. It inserts data in to zipcodes collection.
+ */
 var uploadfile = function(req,res) {
     var filepath =  path.join(approotpath, '/public/data/zipcodes.xlsx');
     var workbook = xlsx.readFile(filepath);
@@ -14,11 +16,11 @@ var uploadfile = function(req,res) {
             var sheetdata = xlsx.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
             if (sheetdata.length > 0) {
                 sheetdata.forEach(function(zipcode){
-                   // zipcodedao.save(zipcode);
+                   //zipcodedao.save(zipcode);
                 });
             }
         });
-       zipcodedao.getAllStates(function(err,states){
+       zipcodedao.getZipCodesForCity('Nashua',function(err,states){
         if(err) {console.log(err);}
            console.log(JSON.stringify(states));
        });
